5bdaee45502683595081b8ea0144a9dda9391ece
peymanhewitt <shewitt@live.co.uk>
2025-02-26 22:12:06 +0000

    alter sql file to add index and change primary key to the unique combination

--- init.sql
+++ init.sql
@@ -6,12 +6,13 @@ CREATE TABLE user (
 
 
 CREATE TABLE user_decisions (
-    id SERIAL PRIMARY KEY,
     actor_id TEXT NOT NULL,
     recipient_id TEXT NOT NULL,
     liked BOOLEAN NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    UNIQUE(actor_id, recipient_id)
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (actor_id, recipient_id),
+    INDEX idx_recipient_liked (recipient_id, liked),
+    INDEX idx_recipient_updated (recipient_id, updated_at, actor_id)
 
 );
